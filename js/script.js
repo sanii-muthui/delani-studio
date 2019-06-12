0308d9ded385e81e4fb1b63d4e9a4b2317552c31
sanii-muthui <muthuisanii@gmail.com>
2019-06-12 11:07:04 +0300

    changed id to div

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-$(document).ready(function(){
-  $("form").submit(function(){
-       var nameInput = $("input#name").val();
-      var emailInput = $("input#email").val();
-      var messageTextArea= $("textArea#message").val();
-       $(".name").text(nameInput);
-        $(".email").text(emailInput);
-        $(".message").text(messageTextArea);
-    alert("submitted");
-      event.preventDefault();
-  });
+$(document).ready(function() {
+// Submit form with id function.
+$("#btn_id").click(function() {
+var name = $("#name").val();
+var email = $("#email").val();
+var message= $("#message").val();
+if (validation()) // Calling validation function.
+{
+$("#form_id").submit(); // Form submission.
+alert("submitted successfully");
+}
 });
 $(document).ready(function(){
-$(.click1).click(function(){
- $(".deisgn-hidden").toggle();
- $(".design-show").toggle();
+$(".imgs").click(function(){
+ $(".d").toggle();
  });
   });
 $(document).ready(function(){
