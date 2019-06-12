5b3e84844e32b0e1944e966d7acb8e620a0bc37d
sanii-muthui <muthuisanii@gmail.com>
2019-06-12 19:33:58 +0300

    changed js fuction

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
-$(document).ready(function() {
-// Submit form with id function.
-$("#btn_id").click(function() {
-var name = $("#name").val();
-var email = $("#email").val();
-var message= $("#message").val();
-if (validation()) // Calling validation function.
-{
-$("#form_id").submit(); // Form submission.
-alert("submitted successfully");
-}
+// $(document).ready(function() {
+// // Submit form with id function.
+// $("#btn_id").click(function() {
+// var name = $("#name").val();
+// var email = $("#email").val();
+// var message= $("#message").val();
+// if (validation()) // Calling validation function.
+// {
+// $("#form_id").submit(); // Form submission.
+// alert("submitted successfully");
+// }
+// });
+$(document).ready(function(){
+  $("#d-click").click(function(){
+    $("#d").toggle();
+  });
 });
 $(document).ready(function(){
-$(".imgs").click(function(){
- $(".d").toggle();
- });
+  $("#p-click").click(function(){
+    $("#p").toggle();
   });
+});
 $(document).ready(function(){
-$(.click2).click(function(){
- $(".development-hidden").toggle();
- $(".development-show").toggle();
- });
+  $("#m-click").click(function(){
+    $("#m").toggle();
   });
-$(document).ready(function(){
-$(.click3).click(function(){
- $(".product-hidden").toggle();
- $(".product-show").toggle();
- });
-  });
+});
+// $(document).ready(function(){
+// $(.click2).click(function(){
+//  $(".development-hidden").toggle();
+//  $(".development-show").toggle();
+//  });
+//   });
+// $(document).ready(function(){
+// $(.click3).click(function(){
+//  $(".product-hidden").toggle();
+//  $(".product-show").toggle();
+//  });
+//   });
