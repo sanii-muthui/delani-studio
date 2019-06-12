10ad636dd330342383d0e6b877f98870fb5dcab1
sanii-muthui <muthuisanii@gmail.com>
2019-06-12 21:17:19 +0300

    what we do js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-// $(document).ready(function() {
-// // Submit form with id function.
-// $("#btn_id").click(function() {
-// var name = $("#name").val();
-// var email = $("#email").val();
-// var message= $("#message").val();
-// if (validation()) // Calling validation function.
-// {
-// $("#form_id").submit(); // Form submission.
-// alert("submitted successfully");
-// }
-// });
 $(document).ready(function(){
+	$("#d").hide();
+	$("#p").hide();
+	$("#m").hide();
+
   $("#d-click").click(function(){
     $("#d").toggle();
   });
@@ -25,15 +17,3 @@ $(document).ready(function(){
     $("#m").toggle();
   });
 });
-// $(document).ready(function(){
-// $(.click2).click(function(){
-//  $(".development-hidden").toggle();
-//  $(".development-show").toggle();
-//  });
-//   });
-// $(document).ready(function(){
-// $(.click3).click(function(){
-//  $(".product-hidden").toggle();
-//  $(".product-show").toggle();
-//  });
-//   });
